ce27807ee3db3bf2053be9f76872a9fa2decc520
Nikola Milinkovic <nikolamilinkovic221@gmail.com>
2024-05-30 22:53:40 +0200

    Test deployment

--- FAWE_string.js
+++ FAWE_string.js
@@ -2,7 +2,7 @@ const path = require('path');
 const fs = require('fs');
 const puppeteer = require('puppeteer');
 require('dotenv').config();
-const puppeteer = require('puppeteer');
+
 
 const getFAWEString = async (originalname, buffer, req, res) => {
   try {
