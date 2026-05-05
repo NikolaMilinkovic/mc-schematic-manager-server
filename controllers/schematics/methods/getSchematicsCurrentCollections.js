0f68ec2cdeb425f698b138ceeb0a3da98baa3535
Nikola Milinkovic <nmilinkovic@ep-entel.com>
2026-05-05 13:50:53 +0200

    sync

--- controllers/schematics/methods/getSchematicsCurrentCollections.js
+++ controllers/schematics/methods/getSchematicsCurrentCollections.js
@@ -1,38 +1,22 @@
-const { User, resolveStudioContext } = require('./shared');
+const Collection = require('../../../models/collection');
 
 async function getSchematicsCurrentCollections(req, res) {
   try {
-    const { ownerUser } = await resolveStudioContext(req);
-    if (!ownerUser) {
-      return res.status(401).json({ message: 'User not found' });
-    }
+    const { id } = req.params;
 
-    const userWithCollections = await User.findById(ownerUser._id).populate({
-      path: 'collections',
-      select: 'name _id schematics',
-    });
+    const collections = await Collection.find(
+      { schematics: id },
+      { _id: 1, name: 1 },
+    ).lean();
 
-    if (!userWithCollections) {
-      return res.status(401).json({ message: 'User not found' });
-    }
-
-    const schematicId = req.params.id;
-    const currentCollections = [];
-
-    for (const collection of userWithCollections.collections) {
-      for (const schematic of collection.schematics) {
-        if (String(schematic) === schematicId) {
-          currentCollections.push({
-            collection_name: collection.name,
-            collection_id: collection._id,
-          });
-        }
-      }
-    }
+    const currentCollections = collections.map((c) => ({
+      collection_id: c._id,
+      collection_name: c.name,
+    }));
 
     return res.json({ currentCollections });
   } catch (err) {
-    return res.status(500).json({ message: 'Error while fetching user data' });
+    return res.status(500).json({ message: 'Error while fetching collections' });
   }
 }
 
